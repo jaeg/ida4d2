98151f3d288cfc48019a81151480be24fc112502
mbennett <mbennett@localhost>
2012-11-18 11:44:20 -0500

    Removed Logger. Added "User"

--- src/ida/ai/Ida.java
+++ src/ida/ai/Ida.java
@@ -1,14 +1,39 @@
 package ida.ai;
 
+import ida.XML.ResponseDatabase;
+import ida.gui.Gui;
+import ida.gui.Voice;
+import ida.responses.Response;
+import ida.user.User;
+import ida.user.UserMessage;
+
 /**
  * Our chatbot. The head of the program.
  * 
  * @author Matt
- *
+ * 
  */
 public class Ida {
 
-	public Ida() {
+	private UserMessage userMessage;
+	private ResponseDatabase responseDatabase;
+	private User user;
 
+	public Ida() {
+		userMessage = new UserMessage();
+		user = new User();
+		try {
+			responseDatabase = new ResponseDatabase();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void respondTo(String input) {
+		userMessage.setMessage(input);
+		Gui.logField.append("\nME: " + input);
+		Response response = responseDatabase.getResponse(userMessage.splitMessageIntoKeywords());
+		Gui.logField.append("\nIDA: " + response);
+		Voice.sayIt(response);
 	}
 }
