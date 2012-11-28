385f507105bf6c7895effb901438ea65c4fb4091
jagodsey <jagodsey@localhost>
2012-11-28 12:23:04 -0500

    Finished up logger

--- src/ida/Logger.java
+++ src/ida/Logger.java
@@ -3,6 +3,7 @@ package ida;
 import java.awt.Dimension;
 
 import javax.swing.BoxLayout;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -11,8 +12,12 @@ public class Logger extends JPanel
 {
 	private static final long serialVersionUID = 1L;
 	static JTextArea log = new JTextArea();
+	private static JFrame logFrame;
 	public Logger()
 	{
+		logFrame = new JFrame("Log window");
+		logFrame.getContentPane().add(this);
+
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		//this.setPreferredSize(new Dimension(300,200));
 		JScrollPane logPane = new JScrollPane(log);
@@ -21,11 +26,20 @@ public class Logger extends JPanel
 		//logPane.add(log);
 		add(logPane);
 		
+		logFrame.pack();
+		logFrame.setVisible(true);
+		logFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
 	}
 	
 	public static void log(String message)
 	{
 		log.append(message);
+	}
+	
+	public static void showLog(boolean flag)
+	{
+		logFrame.setVisible(flag);
 	}
 
 }
