72a4ec4a34c95c8b37e501cf6d9475f529ac8e31
mbennett <mbennett@localhost>
2012-12-01 14:15:42 -0500

    Removed some unused classes.

--- src/ida/Utilities/LanguageUtility.java
+++ src/ida/Utilities/LanguageUtility.java
@@ -4,38 +4,38 @@ import ida.XML.ResponseDatabase;
 import ida.responses.Response;
 
 public class LanguageUtility {
-	
+
 	private String sentence;
-	
-	public LanguageUtility(){
+
+	public LanguageUtility() {
 		this.sentence = null;
 	}
-	
-	public void setSentence(String sentence){
+
+	public void setSentence(String sentence) {
 		this.sentence = sentence;
 	}
-	
-	public boolean specialSentence(){
-		if (sentence.contains("I like") || sentence.contains("I hate") || sentence.contains("My name is")){
+
+	public boolean specialSentence() {
+		if (sentence.contains("I like") || sentence.contains("I hate") || sentence.contains("My name is")) {
 			return true;
 		}
 		return false;
 	}
-	
-	public Response respond(){
+
+	public Response respond() {
 		return new Response(findResponse());
 	}
-	
-	private String findResponse(){
-		if (sentence.contains("I like")){
+
+	private String findResponse() {
+		if (sentence.contains("I like")) {
 			String[] pieces = sentence.split("I like ", 2);
 			String[] strips = pieces[1].split("[\\p{P}]");
 			return "Why do you like " + strips[0] + "?";
-		} else if (sentence.contains("I hate")){
+		} else if (sentence.contains("I hate")) {
 			String[] pieces = sentence.split("I hate ", 2);
 			String[] strips = pieces[1].split("[\\p{P}]");
 			return "Why do you hate " + strips[0] + "?";
-		} else if (sentence.contains("My name is")){
+		} else if (sentence.contains("My name is")) {
 			String[] pieces = sentence.split("My name is ", 2);
 			String[] strips = pieces[1].split("[\\p{P}]");
 			ResponseDatabase.user.setName(strips[0]);
