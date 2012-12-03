dcd7bd2bdcc91a8adc607b1d7d5984c612b57e46
cevans2 <cevans2@localhost>
2012-12-03 13:46:19 -0500

    Made the log window continually scroll down now. Got rid of useless comments and useless strings of code.

--- src/ida/Logger.java
+++ src/ida/Logger.java
@@ -1,6 +1,8 @@
 package ida;
 
 import java.awt.Dimension;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -19,13 +21,17 @@ public class Logger extends JPanel
 	{
 		logFrame = new JFrame("Log window");
 		logFrame.getContentPane().add(this);
-
+		
+		
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		//this.setPreferredSize(new Dimension(300,200));
 		JScrollPane logPane = new JScrollPane(log);
 		logPane.setPreferredSize(new Dimension(370,500));
 		
-		//logPane.add(log);
+		logPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener(){
+			public void adjustmentValueChanged(AdjustmentEvent e){
+			log.select(log.getHeight()+1000,0);
+			}});
+		
 		add(logPane);
 		
 		logFrame.pack();
@@ -46,4 +52,7 @@ public class Logger extends JPanel
 		currentState = !currentState;
 	}
 
+	
+	
 }
+
